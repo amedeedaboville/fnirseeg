616629474262e3d5c350169e55229c401666c11c
Amedee d'Aboville <amedee.daboville@gmail.com>
2013-06-22 11:10:55 -0700

    Fixed arduino connection.

--- chrome/arduino.js
+++ chrome/arduino.js
@@ -3,18 +3,26 @@
  * May 23 2013
  * Amedee d'Aboville
  */
+function parseIncomingLine(dataLine) {
+    var vals = dataLine.split(",");
+    if($(".messageElement").length >20) {
+        $(".messageElement")[0].remove();
+    }
+    $("#recentArduino").append('<li class="messageElement"><a tabindex="-1">'+dataLine+'</a></li>');
+    
+}
 var ab2str=function(buf) { /* Convert an ArrayBuffer to a String, using UTF-8 as the encoding scheme.*/
     return String.fromCharCode.apply(null, new Uint8Array(buf));
 };
 var port = "";
+var dataRead="";
 var onCharRead=function(readInfo) {
-    if (readInfo && readInfo.bytesRead>0 && readInfo.data) {
+    if (readInfo && readInfo.data && readInfo.data.byteLength> 0) {
         var str=ab2str(readInfo.data);
-        if (str[readInfo.bytesRead-1]==='\n') {
-            dataRead+=str.substring(0, readInfo.bytesRead-1);
-            console.log(dataRead);
+        if (str[str.length-1]==='\n') {
+            dataRead+=str.substring(0, readInfo.data.byteLength-2);
             parseIncomingLine(dataRead);
-            return;
+            dataRead="";
         } else {
             dataRead+=str;
         }
@@ -23,10 +31,10 @@ var onCharRead=function(readInfo) {
 }
 var onConnected = function (info) {
     console.log(info);
-    if (info.connectionId != -1)
+    connectionId=info.connectionId;
+    if (connectionId != -1)
     {
         $("#connectionStatus").text("Connected to port "+port);
-        var dataRead='';
         chrome.serial.read(info.connectionId, 128, onCharRead);
     }
     else {
@@ -39,14 +47,20 @@ var connect = function(newPort) {
     $("#connectionStatus").text("Trying to connect to port "+port +".");
     chrome.serial.open(port,{bitrate: 9600},onConnected);
 }
-var onGetPorts = function(ports) {
-    ports.map( function (p) { 
-        var a =  $("#portsList").append('<li><a tabindex="-1" href="#" id="'+p+'">'+p+'</a></li>');
+var fillPortList = function(ports) {
+    console.log("filling port list.");
+    ports.map(function (p) { 
+        var a =  $("#portList").append('<li><a href="#" tabindex="-1" id="'+p+'" class="portElement">'+p+'</a></li>');
         $("a[id='"+p+"']").click(function(e) {
             connect(p);
             e.preventDefault();});
-        console.log(a);
     });
+    $(".portElement").fadeIn(500);
+}
+var onGetPorts = function(ports) {
+    console.log("Ports acquired.");
+    $("#portList").empty();
+    fillPortList(ports);
     port = ports.filter(function (p) { 
         if(p == '/dev/ttyACM0' || p == '/dev/ttyACM1' || p == 'COM0' || p == 'COM1' || p == '/dev/ttyusb0') return true;
         else return false})[0];
