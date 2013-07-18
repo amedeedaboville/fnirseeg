787c7e3c442634f32e8ffef33e8ed226404c7d8d
Amedee d'Aboville <amedee.daboville@gmail.com>
2013-07-17 21:56:17 -0700

    Cleanup.

--- chrome/plot.js
+++ chrome/plot.js
@@ -5,23 +5,12 @@
 class Context() {
 
 }
-function switchToList() {
-    for(i = 0; i < numCharts;i++){
-        var chart = $('#chart'+i);
-        chart.attr("class","span11");
-        chart.appendTo($('#sensors'));
-        charts[i].update();
-    }
-    $('#sensors .row').remove();
-}
+
 function drawHeatMap {
 
 
 }
-$('#toSensors').click(drawHeatMap);
-$('#toList').click(switchToList);
-
-var numCharts = 8;
+var numCharts = numVoxels;
 var charts = Array();
 for(i =0; i < numCharts; i++) {
     var chartdiv  =d3.select("body").append("div")
@@ -29,27 +18,7 @@ for(i =0; i < numCharts; i++) {
     chartdiv.append("div").attr("class","heading").text("Sensor "+i);
     chartdiv.append("svg")
 }
-
-function randomData(points) { //# groups,# points per group
-    random = d3.random.normal();
-    var d1 = [],
-        d2 = [];
-    for (j = 0; j < points; j++) {
-        d1.push({x:j, y: random()});
-        d2.push({x:j, y: random()});
-    }
-    return [
-    {
-        values: d1,
-            key: 'HbO2',
-            color: '#df0000',
-    },{
-        values: d2,
-            key: 'Hb',
-            color: '#3000ff',
-    }];
-}
-for(i =0; i < numCharts; i++) {
+for(i = 0; i < numVoxels; i++) {
     var chart;
     chart = nv.models.lineChart();
     chart.xAxis.tickFormat(d3.format('.02f'));
